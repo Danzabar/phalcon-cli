366b96e907f237e09621a25063cf4eea85b618ff
Dan Cox <danzabian@gmail.com>
2015-01-06 08:04:03 +0000

    Adding validation fail exception class

---
+++ src/Input/Exceptions/ValidationFailException.php
@@ -0,0 +1,63 @@
+<?php namespace Danzabar\CLI\Input\Exceptions;
+
+/**
+ * The exception class for when a validation method fails
+ *
+ * @package CLI
+ * @subpackage Input\Exceptions
+ * @author Dan Cox
+ */
+class ValidationFailException extends \Exception
+{
+	/**
+	 * The tested value
+	 *
+	 * @var string
+	 */
+	protected $value;
+
+	/**
+	 * The tried method
+	 *
+	 * @var string
+	 */
+	protected $method;
+
+	/**
+	 * Fire exception
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function __construct($value, $method, $code = 0, \Exception $previous = NULL)
+	{
+		$this->value = $value;
+		$this->method = $method;
+
+		parent::__construct("Validation method $method failed on value $value", $code, $previous);
+	}
+
+	/**
+	 * Returns the value
+	 *
+	 * @return String
+	 * @author Dan Cox
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	/**
+	 * Returns the method
+	 *
+	 * @return String
+	 * @author Dan Cox
+	 */
+	public function getMethod()
+	{
+		return $this->method;
+	}
+	
+
+} // END class ValidationFailException extends \Exception
---
+++ tests/Input/Exceptions/ValidationFailExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Danzabar\CLI\Input\Exceptions\ValidationFailException;
+
+/**
+ * Test case for the validation fail exception class
+ *
+ * @package CLI
+ * @subpackage Tests\Input
+ * @author Dan Cox
+ */
+class ValidationFailExceptionTest extends \PHPUnit_Framework_TestCase
+{
+	
+	/**
+	 * fire the exception
+	 *
+	 * @return void
+	 * @author Dan Cox
+	 */
+	public function test_fire()
+	{
+		try {
+		
+			throw new ValidationFailException('eheud', 'Email');
+
+		} catch(\Exception $e) {
+			
+			$this->assertEquals('eheud', $e->getValue());
+			$this->assertEquals('Email', $e->getMethod());
+		}
+	}
+
+} // END class ValidationFailExceptionTest extends \PHPUnit_Framework_TestCase
